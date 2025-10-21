4cbd394928f5887a167ede1bfcabeae1d64f2f41
Maxime Loir <maxloir@hotmail.fr>
2025-10-21 16:56:22 +0200

    quete 4

---
+++ src/components1/PokemonCard.tsx
@@ -0,0 +1,15 @@
+function PokemonCard({ pokemon }) {
+
+  return (
+    <figure>
+      {pokemon.imgSrc !== undefined ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
+      <figcaption>{pokemon.name}</figcaption>
+    </figure>
+  );
+}
+
+export default PokemonCard;
