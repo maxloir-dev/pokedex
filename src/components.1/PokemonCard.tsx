1d27580fe8b889001b640b699f6c1cb0a074e431
Maxime Loir <maxloir@hotmail.fr>
2025-10-21 15:44:07 +0200

    quete 3

--- src/components.1/PokemonCard.tsx
+++ src/components.1/PokemonCard.tsx
@@ -1,13 +1,29 @@
-const PokemonCard: React.FC = () => {
+const pokemonList = [
+    {
+        name: "bulbasaur",
+        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {name: "mew",
+
+    },
+];
+
+
+function PokemonCard() {
   return (
-    <figure>
-        <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="Bulbasaur"
-        />
-        <figcaption>Bulbasaur</figcaption>
-    </figure>
+    <div>
+      {pokemonList.map((pokemon, index) => (
+        <figure key={index}>
+          {pokemon.imgSrc !== undefined ? (
+            <img src={pokemon.imgSrc} alt={pokemon.name} />
+          ) : (
+            <p>???</p>
+          )}
+          <figcaption>{pokemon.name}</figcaption>
+        </figure>
+      ))}
+    </div>
   );
-};
+}
 
 export default PokemonCard;
